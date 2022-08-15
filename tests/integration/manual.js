4c31d15c22fb98cce2e88f2612ebf125a8f9672b
James Tierney <james.tierney384@gmail.com>
2022-08-15 09:38:32 +0100

    Co-authored-by: si-mv <si-mv@users.noreply.github.com>

--- tests/integration/manual.js
+++ tests/integration/manual.js
@@ -6,8 +6,6 @@ async function main () {
     // sync the whole database
     await db.sync({ force: true })
 
-
-
     // create some bands
     const metalica = await Band.create({
         name: 'Metalica',
@@ -49,6 +47,26 @@ async function main () {
     await kiss.addSong(thunderstruck)
     await kiss.addSong(verge)
 
+    /**
+     * EAGER LOADING
+     */
+
+    // later on in our project, suppose we want to query the db for the kiss band
+    const kissQuery = await Band.findOne({
+        where: { name: 'Kiss' }
+    })
+    // notice that the returned object does not also have the songs for kiss!
+    console.log('\n\nWithout songs:\n', kissQuery.toJSON())
+
+    // however, if we pass an include key, we can get the songs as well
+    const kissQueryWithSongs = await Band.findOne({
+        where: { name: 'Kiss' },
+        include: Song // pass the instance of Song to get kiss' songs
+    })
+    console.log('\n\nWith songs:\n', kissQueryWithSongs.toJSON())
+
 }
+
+
 
 main()
